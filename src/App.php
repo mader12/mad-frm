448073e4848b08c605246fd7af3665e7f76d1d2e
Drozhzhinov <mader12@yandex.ru>
2018-07-09 13:00:46 +0300

    fix

--- src/App.php
+++ src/App.php
@@ -2,9 +2,15 @@
 namespace Mad;
 
 class App {
+
+    public $test = 0;
     
-    public function __constructor () {
-       
+    public function __construct ($config) {
+       $this->test = $config;
+    }
+
+    public function run() {
+        echo 1;
     }
 }
 
